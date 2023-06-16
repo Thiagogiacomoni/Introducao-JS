d57decc8e72ba537d68b2cff1e66e2957302d856
Thiagogiacomoni <giacomoni201820@gmail.com>
2023-06-16 11:14:39 -0300

    att

---
+++ aula006/index.js
@@ -0,0 +1,17 @@
+/* 
+* Não podemos criar variáveis com palavras reservadas;
+* Variáveis precisam ter nomes significativos;
+* Não pode começar o nome de uma variável com um número;
+* Não podem conter espaços e traços;
+* Utilizamos camelCase;
+* Não podemos redeclarar variáveis com let;
+* Não UTILIZE VAR, UTILIZE LET.
+*/
+
+let nome = 'Thiago';
+
+console.log(`${nome} nasceu em 1999;`);
+console.log('Tem 24 anos atualmente;');
+console.log('Estuda desenvolvimento Web;');
+console.log('Gosta de Basquete;');
+console.log('Hoje está chovendo.');
---
+++ aula007/index.js
@@ -0,0 +1,16 @@
+/* 
+* Não podemos criar constantes com palavras reservadas;
+* Constantes precisam ter nomes significativos;
+* Não pode começar o nome de uma constante com um número;
+* Não podem conter espaços e traços;
+* Utilizamos camelCase;
+* Não podemos modificar o valor de uma constante;
+* Não UTILIZE VAR, UTILIZE CONST.
+*/
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero + segundoNumero;
+const resultadoDuplicado = resultado * 2;
+
+console.log(resultadoDuplicado)
