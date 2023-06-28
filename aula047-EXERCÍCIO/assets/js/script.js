7b00b0fb6dbf495267c7888a38f9045744212207
Thiago Giacomoni <giacomoni201820@gmail.com>
2023-06-28 18:28:50 -0300

    att

---
+++ aula047-EXERCÍCIO/assets/js/script.js
@@ -0,0 +1,14 @@
+const relogio = document.querySelector('#relogio');
+const iniciar = document.querySelector('#iniciar');
+const pausar = document.querySelector('#pausar');
+const zerar = document.querySelector('#zerar');
+
+iniciar.addEventListener('click', function(event) {
+    alert('cliquei');
+});
+pausar.addEventListener('click', function(event) {
+    alert('cliquei');
+});
+zerar.addEventListener('click', function(event) {
+    alert('cliquei');
+});
