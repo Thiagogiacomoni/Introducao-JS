cd757b908fd38d5bfeed8770a9a1e7a7e4337a70
Thiagogiacomoni <giacomoni201820@gmail.com>
2023-08-12 02:10:49 -0300

    att

--- node/aula02/app.js
+++ node/aula02/app.js
@@ -1,4 +1,11 @@
 const Cachorro = require('./z/mod2');
-
 const c1 = new Cachorro('dog');
 c1.latir();
+
+// const path = require('path');
+// console.log(__dirname)
+// console.log(path.resolve(__dirname, '..', '..', 'arquivos', 'imagens'));
+
+
+// console.log(__filename); // nome do arquivo atual
+// console.log(__dirname); // noma da pasta atual
