1127938c8bd42ffb4212ba6bf35406e89c793f9f
Thiago Giacomoni <giacomoni201820@gmail.com>
2023-11-08 20:29:08 -0300

    att

---
+++ node/aula06/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+// CRUD -> CREATE, READ, UPDATE, DELETE
+//          POST   GET    PUT    DELETE
+
+app.get('/', (req, res) =>  {
+    res.send(`
+    <form action="/" method="POST">
+    Nome: <input type="texte" name="nome">
+    <button>ENVIAR</button>
+    </form>
+    `);
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi o formulário');
+})
+
+app.get('/contato', (req, res) => {
+    res.send('Obrigado por entrar em contato!');
+});
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000')
+    console.log('Server ligado!');
+});
